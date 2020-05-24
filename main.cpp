08df45e45a16acd5cdc332add65f9e70b4916f51
Egor Kirillov <egor.kirillov@iqoption.com>
2020-05-24 17:57:27 +0300

    added TimeStamp type

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+#include "ebmlio/Storage.h"
+#include "ebmlio/logger/log.h"
+#include "ebmlio/dataTypes/stdSupport.h"
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    ebmlio::Storage s;
+
+    std::string testString {"This is the string for tests"};
+    ebmlio::TimeStamp time = std::time(nullptr);
+
+    s.save(time, testString);
+    auto resultString = s.load<std::string>(time).value_or("");
+
+    LOG(testString);
+    LOG(resultString);
+
     return 0;
 }
