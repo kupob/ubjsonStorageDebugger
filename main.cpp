a036a34ea754f984a50b4b41bc24b40fcb2f1ed0
egor.kirillov <egor.kirillov@iqoption.com>
2020-05-31 18:17:26 +0300

    improved log system; separate storage files by limit; loadRange

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
+#include <chrono>
 
 #include "ubjsonStorage/Storage.h"
 #include "ubjsonStorage/logger/log.h"
 #include "ubjsonStorage/dataTypes/stdSupport.h"
 
-int main()
+bool test1(storage::Storage& s) ///< Store two strings separately and load it back
 {
-    storage::Storage s;
-
     std::string testString {"This is the first string for tests"};
     std::string testString2 {"This is the second string for tests"};
 
-    storage::TimeStamp time = std::time(nullptr);
+    auto now = std::chrono::system_clock::now();
+    auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
+    storage::TimeStamp time = now_ms.time_since_epoch().count();
     storage::TimeStamp time2 = time + 1;
 
     s.save(time, testString);
@@ -20,11 +21,22 @@ int main()
     auto resultString = s.load<std::string>(time).value_or("Something went wrong");
     auto resultString2 = s.load<std::string>(time2).value_or("Something went wrong");
 
-    LOG(testString);
-    LOG(resultString);
+    if (testString == resultString &&
+        testString2 == resultString2) {
+        LOG ("Test 1 passed");
+    }
+    else {
+        LOGERROR("Test 1 not passed");
+        LOGERROR ("src: " + testString + " result: " + resultString);
+        LOGERROR ("src: " + testString2 + " result: " + resultString2);
+    }
+}
 
-    LOG(testString2);
-    LOG(resultString2);
+int main()
+{
+    storage::Storage s;
+
+    test1(s);
 
     return 0;
 }
